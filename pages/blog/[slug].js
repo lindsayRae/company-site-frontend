74943d8c431b70e61c0c364205f51ad307dce4c4
Lindsay <lbarnett712@gmail.com>
2021-06-24 06:56:14 -0400

    starter next files

---
+++ pages/blog/[slug].js
@@ -0,0 +1,7 @@
+export default function SingleBlogPage() {
+  return (
+    <div>
+      <h1>Blog #1</h1>
+    </div>
+  );
+}
---
+++ pages/portfolio/[slug].js
@@ -0,0 +1,7 @@
+export default function SinglePortfolioPage() {
+  return (
+    <div>
+      <h1>Portfolio #1</h1>
+    </div>
+  );
+}
