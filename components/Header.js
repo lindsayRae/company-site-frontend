75a1c2957ba51ca55f200a6e21120d44771d0402
Lindsay <lbarnett712@gmail.com>
2021-07-02 07:01:08 -0400

    added bootstrap containers

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '../styles/Header.module.css';
+import styles from '@/styles/Header.module.css';
 // https://www.youtube.com/watch?v=6S52Oywgdlk&t=151s
 import {
   Container,
@@ -17,21 +17,26 @@ import {
 export default function Header() {
   return (
     <header>
-     <Navbar bg="light" expand="lg" className="">
-      <Navbar.Brand href="/">Lindsay's Co</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-        <Nav className="mr-5">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="/blog">Blog</Nav.Link> 
-        </Nav>
-        <Nav className="">         
-          <Nav.Link href="#link">Log In</Nav.Link>
-          <Button variant="outline-success">Contact Us</Button>         
-        </Nav>   
-      </Navbar.Collapse>
-    </Navbar>
+    
+        <Navbar bg="light" variant="light" expand="lg" fixed="top" >
+        <Container>
+          <Navbar.Brand href="/">Lindsay's Co</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav className="mr-5">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="/blog">Blog</Nav.Link> 
+            </Nav>
+            <Nav className="">         
+              <Nav.Link href="#link">Log In</Nav.Link>
+              <Button variant="outline-success">Contact Us</Button>         
+            </Nav>   
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      
+     
     </header>
   );
 }
