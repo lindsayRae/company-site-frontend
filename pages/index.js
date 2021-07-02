b261d0128e8f681e125e7f0e547fd44de6aa72da
Lindsay <lbarnett712@gmail.com>
2021-07-02 07:05:41 -0400

    added jumbotron

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
-import Layout from '../components/Layout';
 
+import Layout from '@/components/Layout';
+import { Jumbotron, Container, Row, Col, Button } from 'react-bootstrap';
+import styles from '@/styles/Home.module.css'
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1>Home</h1> 
+       <Jumbotron className={styles.showcase} >
+        <Container>
+          <Row>
+          <Col md={{ span: 6, offset: 6 }}>
+            <h1 className="header">We Listen, Collaborate & Deliver to get your Company where it needs to go.</h1>
+            <p>We create custom, scalable web applications.</p>
+            <Button variant="primary">Tell us about your project</Button>
+          </Col>            
+          </Row>       
+        </Container>        
+      
+    </Jumbotron>
     </Layout>
   );
 }
