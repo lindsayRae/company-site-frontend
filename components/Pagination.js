471fce3b93cda9c27fc6fd068b947fbed0fe6722
Lindsay <lbarnett712@gmail.com>
2021-07-06 10:30:46 -0400

    added pagination as component

---
+++ components/Pagination.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { PER_PAGE } from '@/config/index';
+
+export default function Pagination({ page, total }) {
+  const lastPage = Math.ceil(total / PER_PAGE);
+  return (
+    <>
+      {page > 1 && (
+        <Link href={`/blog?page=${page - 1}`}>
+          <a>Prev</a>
+        </Link>
+      )}
+
+      {page < lastPage && (
+        <Link href={`/blog?page=${page + 1}`}>
+          <a>Next</a>
+        </Link>
+      )}
+    </>
+  );
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,32 +1,45 @@
 import Layout from '@/components/Layout';
-import { API_URL } from '@/config/index';
+import { API_URL, PER_PAGE } from '@/config/index';
 import BlogItem from '@/components/BlogItem';
 import SearchBlog from '@/components/SearchBlog';
+import Pagination from '@/components/Pagination';
 
-export default function BlogPage(blogs) {
-  console.log(blogs);
+export default function BlogPage({ blogs, page, total }) {
   return (
     <Layout>
       <h1 style={{ marginTop: '50px' }}>My Blog</h1>
       <SearchBlog />
       {blogs.length === 0 && <h3>No blogs to show</h3>}
 
-      {blogs.blogs.map((b) => (
+      {blogs.map((b) => (
         <BlogItem key={b.id} b={b} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   );
 }
 
 //? runs server side
-//export async function getServerSideProps(){
-//? run at build time will run static pages
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/blogs?_sort=date:ASC&_limit=6`);
-  const blogs = await res.json();
-  console.log(blogs);
+export async function getServerSideProps({ query: { page = 1 } }) {
+  console.log(page);
+  //? run at build time will run static pages
+  //export async function getStaticProps() {
+
+  //? Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+  //? Fetch total/count
+  const totalRes = await fetch(`${API_URL}/blogs/count`);
+  const total = await totalRes.json();
+
+  //? Fetch Blogs
+  const blogRes = await fetch(
+    `${API_URL}/blogs?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const blogs = await blogRes.json();
+
   return {
-    props: { blogs },
-    revalidate: 1,
+    props: { blogs, page: +page, total },
   };
 }
