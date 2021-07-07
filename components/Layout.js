c64dd1d6f6a42ba1bc73de308eed58a8309ced80
Lindsay <lbarnett712@gmail.com>
2021-07-07 07:51:41 -0400

    cleanup

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,8 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router'
-import styles from '@/styles/Layout.module.css';
 import Header from './Header';
 import Footer from './Footer';
-import Showcase from './Showcase';
-
 
 export default function Layout({ title, keywords, description, children }) {
-  const router = useRouter()
-
   return (
     <div>
       <Head>
@@ -17,15 +11,12 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
-        <title>{title}</title>       
+        <title>{title}</title>
       </Head>
-    
+
       <Header />
-      {/* {router.pathname === '/' && <Showcase />} */}
       <div>{children}</div>
       <Footer />
-     
-    
     </div>
   );
 }
