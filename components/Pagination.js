52589c47e22d5c31f17eeb0c6a4ac1249be9bc25
Lindsay <lbarnett712@gmail.com>
2021-07-12 07:39:07 -0400

    added pagination styles

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,21 +1,28 @@
 import Link from 'next/link';
 import { PER_PAGE } from '@/config/index';
+import { Col, Row } from 'react-bootstrap';
+import styles from '@/styles/Pagination.module.css';
 
 export default function Pagination({ page, total }) {
   const lastPage = Math.ceil(total / PER_PAGE);
-  return (
-    <>
-      {page > 1 && (
-        <Link href={`/blog?page=${page - 1}`}>
-          <a>Prev</a>
-        </Link>
-      )}
 
-      {page < lastPage && (
-        <Link href={`/blog?page=${page + 1}`}>
-          <a>Next</a>
-        </Link>
-      )}
-    </>
+  return (
+    <Row>
+      <Col>
+        {page > 1 && (
+          <Link href={`/blog?page=${page - 1}`}>
+            <a className={styles.pagBtn}>Prev</a>
+          </Link>
+        )}
+      </Col>
+
+      <Col className='text-right'>
+        {page < lastPage && (
+          <Link href={`/blog?page=${page + 1} `}>
+            <a className={styles.pagBtn}>Next</a>
+          </Link>
+        )}
+      </Col>
+    </Row>
   );
 }
