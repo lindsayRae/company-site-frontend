a855e4f701f7b634fb10e760f9e0302452a9ebdc
Lindsay <lbarnett712@gmail.com>
2021-07-13 12:18:41 -0400

    finished faq layout

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,68 +1,73 @@
 import Layout from '@/components/Layout';
-import styles from '@/styles/Home.module.css';
 import { Container, Row, Col, Accordion, Card } from 'react-bootstrap';
+//import Accordion from 'react-bootstrap/Accordion';
+import Link from 'next/link';
+import styles from '@/styles/Home.module.css';
 
-import { FaCaretDown } from 'react-icons/fa';
+import { FaAngleDown } from 'react-icons/fa';
 
 export default function FAQPage() {
   return (
     <Layout>
-      <h1 style={{ marginTop: '50px' }}>My FAQ</h1>
-
-      {/**********************
-                 FAQs
-        ***********************/}
-
-      <section className='faqs welcome-services ptb-120 bg-38d16a'>
+      <div className='page-title-area item-bg1'>
         <Container>
-          <div className='section-title'>
-            <h2>FAQ's</h2>
+          <h1>FAQs</h1>
+          <ul>
+            <li>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>FAQ</li>
+          </ul>
+        </Container>
+      </div>
+
+      <section className='faq-area ptb-120'>
+        <Container>
+          <div className='faq-accordion'>
+            <Accordion defaultActiveKey='0'>
+              <Card>
+                <Accordion.Toggle
+                  as={Card.Header}
+                  eventKey='0'
+                  className={styles.expand}
+                >
+                  <FaAngleDown size={28} className='mr-2' />
+                  This is a question...
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='0'>
+                  <Card.Body>Hello! Here is the answer</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle
+                  as={Card.Header}
+                  eventKey='1'
+                  className={styles.expand}
+                >
+                  <FaAngleDown size={28} className='mr-2' />
+                  This is a question...
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='1'>
+                  <Card.Body>Hello! I'm another body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle
+                  as={Card.Header}
+                  eventKey='2'
+                  className={styles.expand}
+                >
+                  <FaAngleDown size={28} className='mr-2' />
+                  This is a question...
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='2'>
+                  <Card.Body>Hello! I'm another body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
           </div>
-          <Row>
-            <Col>
-              <Accordion defaultActiveKey='0'>
-                <Card>
-                  <Accordion.Toggle
-                    as={Card.Header}
-                    eventKey='0'
-                    className={styles.expand}
-                  >
-                    This is a question...
-                    <FaCaretDown size={28} />
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey='0'>
-                    <Card.Body>Hello! Here is the answer</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-                <Card>
-                  <Accordion.Toggle
-                    as={Card.Header}
-                    eventKey='1'
-                    className={styles.expand}
-                  >
-                    This is a question...
-                    <FaCaretDown size={28} />
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey='1'>
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-                <Card>
-                  <Accordion.Toggle
-                    as={Card.Header}
-                    eventKey='2'
-                    className={styles.expand}
-                  >
-                    This is a question...
-                    <FaCaretDown size={28} />
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey='2'>
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion>
-            </Col>
-          </Row>
         </Container>
       </section>
     </Layout>
