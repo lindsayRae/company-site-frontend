a0c4a9d6d76d5270231dea0fffca401697306b94
Lindsay <lbarnett712@gmail.com>
2021-08-05 12:07:38 -0400

    added sendgrid and contact us email handling

---
+++ pages/api/mail.js
@@ -0,0 +1,27 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default (req, res) => {
+  const body = JSON.parse(req.body);
+  console.log(body);
+
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Phone: ${body.phone}\r\n
+    Subject: ${body.subject}\r\n
+    message: ${body.message}\r\n
+  `;
+
+  const data = {
+    to: 'lbarnett712@gmail.com',
+    from: 'lbarnett712@gmail.com',
+    subject: 'hello test world',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  };
+
+  mail.send(data);
+  res.status(200).json({ status: 'OK' });
+};
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -12,43 +12,53 @@ export default function ContactPage() {
   const [phone, setPhone] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log('heard');
 
-    if (!name) {
-      setError('You must include a name');
-      return;
-    }
-    if (!email) {
-      setError('You must include a email');
-      return;
-    }
-    if (!phone) {
-      setError('You must include a phone');
-      return;
-    }
-    if (!subject) {
-      setError('You must include a subject');
-      return;
-    }
-    if (!message) {
-      setError('You must include a message');
-      return;
+    const formData = {};
+
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      if (!field.value) setError('Please fill in all form fields');
+      formData[field.name] = field.value;
+    });
+    console.log(formData);
+
+    try {
+      const response = await fetch('/api/mail', {
+        method: 'post',
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log('data from api: ', data);
+      if (data.status !== 'OK') {
+        setError(data.message);
+        return;
+      } else {
+        console.log('much success');
+        setEmailSent(true);
+      }
+    } catch (err) {
+      console.log('err: ', err);
+      setError('Something went wrong: ', err);
     }
 
-    let body = {
-      name: name,
-      email: email,
-      phone: phone,
-      subject: subject,
-      message: message,
-    };
+    // let body = {
+    //   name: name,
+    //   //  email: email,
+    //   //  phone: phone,
+    //   //  subject: subject,
+    //   message: message,
+    // };
 
-    console.log(body);
+    // console.log(body);
+    // //! https://www.youtube.com/watch?v=QrVYLLpoyMw
     // try {
-    //   const response = await fetch('/api/contact', {
+    //   const response = await fetch('http://localhost:8082/email/send', {
     //     method: 'POST',
     //     headers: {
     //       'Content-type': 'application/json',
@@ -57,16 +67,17 @@ export default function ContactPage() {
     //   });
 
     //   const data = await response.json();
-
+    //   console.log('data from api: ', data);
     //   if (data.status !== 200) {
     //     setError(data.message);
     //     return;
     //   } else {
-    //     setMessageSent(true);
+    //     //  setMessageSent(true);
     //   }
     // } catch (err) {
+    //   console.log('err: ', err);
     //   setError('Something went wrong: ', err);
-    //   setMessageSent(false);
+    //   // setMessageSent(false);
     // }
   };
 
@@ -124,94 +135,107 @@ export default function ContactPage() {
               </Row>
             </Col>
             <Col lg='6' md='12'>
-              <form onSubmit={handleSubmit}>
-                <Row>
-                  <Col lg='12'>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        className='form-control'
-                        placeholder='Name'
-                        autoComplete='off'
-                        value={name}
-                        onChange={(event) => {
-                          setError('');
-                          setName(event.target.value);
-                        }}
-                      />
-                    </div>
-                  </Col>
-                  <Col lg='12'>
-                    <div className='form-group'>
-                      <input
-                        type='email'
-                        className='form-control'
-                        placeholder='Email'
-                        autoComplete='off'
-                        value={email}
-                        onChange={(event) => {
-                          setError('');
-                          setEmail(event.target.value);
-                        }}
-                      />
-                    </div>
-                  </Col>
-                  <Col lg='12'>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        className='form-control'
-                        placeholder='Phone'
-                        autoComplete='off'
-                        value={phone}
-                        onChange={(event) => {
-                          setError('');
-                          setPhone(event.target.value);
-                        }}
-                      />
-                    </div>
-                  </Col>
-                  <Col lg='12'>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        className='form-control'
-                        placeholder='Subject'
-                        autoComplete='off'
-                        value={subject}
-                        onChange={(event) => {
-                          setError('');
-                          setSubject(event.target.value);
-                        }}
-                      />
-                    </div>
-                  </Col>
-                  <Col lg='12'>
-                    <div className='form-group'>
-                      <textarea
-                        className='form-control'
-                        placeholder='Your Message'
-                        cols='30'
-                        rows='5'
-                        autoComplete='off'
-                        value={message}
-                        onChange={(event) => {
-                          setError('');
-                          setMessage(event.target.value);
-                        }}
-                      ></textarea>
-                    </div>
-                  </Col>
-                  {error && (
-                    <Col lg={12}>
-                      <p className={styles.textRed}>{error}</p>
+              {!emailSent && (
+                <form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col lg='12'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className='form-control'
+                          placeholder='Name'
+                          autoComplete='off'
+                          value={name}
+                          name='name'
+                          onChange={(event) => {
+                            setError('');
+                            setName(event.target.value);
+                          }}
+                        />
+                      </div>
                     </Col>
-                  )}
-                  <Col lg='12'>
-                    <Button type='submit'>Send Message</Button>
-                  </Col>
-                </Row>
-              </form>
+                    <Col lg='12'>
+                      <div className='form-group'>
+                        <input
+                          type='email'
+                          className='form-control'
+                          placeholder='Email'
+                          autoComplete='off'
+                          name='email'
+                          value={email}
+                          onChange={(event) => {
+                            setError('');
+                            setEmail(event.target.value);
+                          }}
+                        />
+                      </div>
+                    </Col>
+                    <Col lg='12'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className='form-control'
+                          placeholder='Phone'
+                          autoComplete='off'
+                          value={phone}
+                          name='phone'
+                          onChange={(event) => {
+                            setError('');
+                            setPhone(event.target.value);
+                          }}
+                        />
+                      </div>
+                    </Col>
+                    <Col lg='12'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className='form-control'
+                          placeholder='Subject'
+                          autoComplete='off'
+                          value={subject}
+                          name='subject'
+                          onChange={(event) => {
+                            setError('');
+                            setSubject(event.target.value);
+                          }}
+                        />
+                      </div>
+                    </Col>
+                    <Col lg='12'>
+                      <div className='form-group'>
+                        <textarea
+                          className='form-control'
+                          placeholder='Your Message'
+                          cols='30'
+                          rows='5'
+                          autoComplete='off'
+                          value={message}
+                          name='message'
+                          onChange={(event) => {
+                            setError('');
+                            setMessage(event.target.value);
+                          }}
+                        ></textarea>
+                      </div>
+                    </Col>
+                    {error && (
+                      <Col lg={12}>
+                        <p className={styles.textRed}>{error}</p>
+                      </Col>
+                    )}
+                    <Col lg='12'>
+                      <Button type='submit'>Send Message</Button>
+                    </Col>
+                  </Row>
+                </form>
+              )}
+              {emailSent && (
+                <p>
+                  Your email has been set. We will respond within 24 hrs during
+                  normal business hours. Thank you!{' '}
+                </p>
+              )}
             </Col>
           </div>
         </Container>
